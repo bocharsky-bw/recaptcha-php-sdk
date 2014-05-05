3b18dda182be028d471cc78002e11a60ec6b9f7a
Victor <bocharsky.bw@gmail.com>
2014-05-05 14:10:57 +0300

    Add method for answer checking

--- examples/index.php
+++ examples/index.php
@@ -2,8 +2,19 @@
 
 include_once __DIR__ .'/../src/BW/Captcha/Recaptcha.php';
 
-$re = new \BW\Captcha\Recaptcha('6LcT9fESAAAAADYP6P-m718_8jW3HAtFF-2OkkuU', '6LcT9fESAAAAACU1kJwoJZBOOqADiyL-KATRCIYi');
+$re = new \BW\Captcha\Recaptcha(
+        '6LcO__ISAAAAACKCCgXgtlqLa1xD2rZtYA0MzY5g', // input your public key here
+        '6LcO__ISAAAAACZwnTN8zOrB3TdeA74mclRqsrpL'  // input your private key here
+    );
 
+$response = '';
+if (0 === strcasecmp($_SERVER['REQUEST_METHOD'], 'POST')) {
+    if ($re->checkAnswer()->isValid()) {
+        $response = 'Congrats! Valid input.';
+    } else {
+        $response = 'Error occurred: ' . $re->getError();
+    }
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,7 +23,12 @@ $re = new \BW\Captcha\Recaptcha('6LcT9fESAAAAADYP6P-m718_8jW3HAtFF-2OkkuU', '6Lc
     <title>reCAPTCHA</title>
 </head>
 <body><!-- the body tag is required or the CAPTCHA may not show on some browsers -->
-    <form method="post" action="verify.php">
+    <?php if ($response) : ?>
+        <h1><?php print $response ?></h1>
+    <?php else : ?>
+        <h1>Please, enter the text from image</h1>
+    <?php endif ?>
+    <form method="post" action="">
         <?php print $re->getHtml(); ?>
         <input type="submit" />
     </form>
--- src/BW/Captcha/Recaptcha.php
+++ src/BW/Captcha/Recaptcha.php
@@ -10,6 +10,9 @@ namespace BW\Captcha;
 class Recaptcha
 {
 
+    /**
+     * Current version of reCAPTCHA PHP SDK
+     */
     const VERSION = '1.11.0';
 
     /**
@@ -36,15 +39,35 @@ class Recaptcha
      * @var string The client private key
      */
     private $privateKey;
+    
+    /**
+     * @var boolean Whether valid user input or no
+     */
+    protected $valid = false;
+    
+    /**
+     * @var string The occurred error
+     */
+    protected $error;
 
 
+    /**
+     * The constructor of objects
+     * 
+     * @param string $publicKey The client public key
+     * @param string $privateKey The client private key
+     */
     public function __construct($publicKey = '', $privateKey = '')
     {
         $this->setPublicKey($publicKey);
         $this->setPrivateKey($privateKey);
     }
     
-    public function __toString() {
+    /**
+     * @see $this->getHtml()
+     */
+    public function __toString()
+    {
         return $this->getHtml();
     }
 
@@ -53,22 +76,18 @@ class Recaptcha
      * Gets the challenge HTML (javascript and non-javascript version).
      * This is called from the browser, and the resulting reCAPTCHA HTML widget
      * is embedded within the HTML form it was called from.
-     * @param string $publicKey A public key for reCAPTCHA
+     * 
      * @param string $error The error given by reCAPTCHA (optional, default is null)
      * @param boolean $use_ssl Should the request be made over ssl? (optional, default is false)
-
-     * @return string - The HTML to be embedded in the user's form.
+     * @return string The HTML to be embedded in the user's form.
+     * @throws \Exception
      */
-    public function getHtml($publicKey = null, $error = null, $use_ssl = false) {
-        if ($publicKey) {
-            $this->setPublicKey($publicKey);
-        }
-
+    public function getHtml($error = null, $use_ssl = false) {
         if ( ! $this->getPublicKey()) {
             throw new \Exception('To use reCAPTCHA you must get an API key from <a href="https://www.google.com/recaptcha/admin/create">https://www.google.com/recaptcha/admin/create</a>');
         }
 
-        if ($use_ssl) {
+        if (true === $use_ssl) {
             $server = self::RECAPTCHA_API_SECURE_SERVER;
         } else {
             $server = self::RECAPTCHA_API_SERVER;
@@ -87,9 +106,128 @@ class Recaptcha
                 .'</noscript>'. PHP_EOL;
     }
 
+    /**
+     * Calls an HTTP POST function to verify if the user's guess was correct
+     * 
+     * @param array $extraParams An array of extra variables to post to the server
+     * @return $this
+     * @throws \Exception
+     */
+    public function checkAnswer($extraParams = array()) {
+        $remoteIp = $_SERVER["REMOTE_ADDR"];
+        $challenge = $_POST["recaptcha_challenge_field"];
+        $response = $_POST["recaptcha_response_field"];
+        
+        if ( ! $this->getPrivateKey()) {
+            throw new \Exception("To use reCAPTCHA you must get an API key from <a href='https://www.google.com/recaptcha/admin/create'>https://www.google.com/recaptcha/admin/create</a>");
+        }
+
+        if ( ! $remoteIp) {
+            throw new \Exception("For security reasons, you must pass the remote ip to reCAPTCHA");
+        }
+
+        if ( ! $challenge) { // discard spam submissions
+            $this->valid = false;
+            $this->error = 'incorrect-captcha-sol';
+        } else { // or check user input
+            $response = $this->httpPost(self::RECAPTCHA_VERIFY_SERVER, '/recaptcha/api/verify',
+                    array(
+                        'privatekey' => $this->getPrivateKey(),
+                        'remoteip' => $remoteIp,
+                        'challenge' => $challenge,
+                        'response' => $response,
+                    ) + $extraParams
+                );
+
+            $answers = explode("\n", $response[1]);
+            if (trim($answers[0]) == 'true') {
+                $this->valid = true;
+            } else {
+                $this->valid = false;
+                $this->error = $answers[1];
+            }
+        }
+        
+        return $this;
+    }
 
     /**
-     * @return string
+     * Submits an HTTP POST to a reCAPTCHA server
+     * 
+     * @param string $host
+     * @param string $path
+     * @param array $data
+     * @param int port
+     * @return array response
+     * @throws \Exception
+     */
+    protected function httpPost($host, $path, array $data, $port = 80) {
+        $req = $this->qsencode($data);
+
+        $http_request  = "POST {$path} HTTP/1.0\r\n";
+        $http_request .= "Host: {$host}\r\n";
+        $http_request .= "Content-Type: application/x-www-form-urlencoded;\r\n";
+        $http_request .= "Content-Length: " . strlen($req) . "\r\n";
+        $http_request .= "User-Agent: reCAPTCHA/PHP\r\n";
+        $http_request .= "\r\n";
+        $http_request .= $req;
+
+        $response = '';
+        if(false == ($fs = @fsockopen($host, (int)$port, $errno, $errstr, 10))) {
+            throw new \Exception('Could not open socket');
+        }
+
+        fwrite($fs, $http_request);
+        while ( ! feof($fs)) {
+            $response .= fgets($fs, 1160); // One TCP-IP packet
+        }
+        fclose($fs);
+
+        $response = explode("\r\n\r\n", $response, 2);
+        return $response;
+    }
+    
+    /**
+     * Encodes the given data into a query string format
+     * 
+     * @param array $data An array of string elements to be encoded
+     * @return string Encoded request
+     */
+    protected function qsencode(array $data = array()) {
+        $req = '';
+        
+        if ($data) {
+            foreach ($data as $key => $value) {
+                $req .= $key . '=' . urlencode(stripslashes($value)) . '&';
+            }
+            $req = substr($req, 0, strlen($req) - 1); // Cut the last '&'
+        }
+        
+        return $req;
+    }
+    
+    /**
+     * Check user input
+     * 
+     * @return bool Whether valid user input or no
+     */
+    public function isValid() {
+        return $this->valid;
+    }
+    
+    /**
+     * Get occurred error
+     * 
+     * @return string The occurred error
+     */
+    public function getError() {
+        return $this->error;
+    }
+    
+    /**
+     * Get the public key
+     * 
+     * @return string The public key
      */
     public function getPublicKey()
     {
@@ -97,7 +235,9 @@ class Recaptcha
     }
 
     /**
-     * @param string $publicKey
+     * Set the public key
+     * 
+     * @param string $publicKey The public key
      * @return $this
      */
     public function setPublicKey($publicKey)
@@ -107,7 +247,9 @@ class Recaptcha
     }
 
     /**
-     * @return string
+     * Get the private key
+     * 
+     * @return string The private key
      */
     public function getPrivateKey()
     {
@@ -115,7 +257,9 @@ class Recaptcha
     }
 
     /**
-     * @param string $privateKey
+     * Set the private key
+     * 
+     * @param string $privateKey The private key
      * @return $this
      */
     public function setPrivateKey($privateKey)
@@ -123,6 +267,5 @@ class Recaptcha
         $this->privateKey = (string)$privateKey;
         return $this;
     }
-
 
 }
